d8fa9c229444c1e71cd80ad3738ae8d480694c83
jaecheon <jjocojusu1@gmail.com>
2019-08-10 16:21:37 +0900

    product post and get All

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,25 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const productModel = require('../models/product');
 
 
-
-
+// 전체 데이터 불러오기.
 router.get('/', (req, res) => {
-    res.status(200).json({
-        msg: 'data 불러옴.'
-    });
+    // res.status(200).json({
+    //     msg: 'data 불러옴.'
+    // });
+    productModel
+        .find()
+        .then(docs => {
+            res.status(201).json({
+                msg: "successful get all data",
+                count: docs.length,
+                products: docs
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
+
+// product data 등록.
 router.post('/', (req, res) => {
 
-    const product = {
+    const product = new productModel({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(200).json({
-        msg: 'data 등록됨.',
-        createProduct: product
     });
+
+    product
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                msg: "successfull product Post",
+                createProduct: result
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+
+
 });
 
 router.patch('/', (req, res) => {
