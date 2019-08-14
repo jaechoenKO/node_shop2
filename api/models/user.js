5463b20d30dd0a2e545b3b02fd691c21ad3adcea
jaecheon <jjocojusu1@gmail.com>
2019-08-14 22:26:09 +0900

    기존 이메일 유무 체크 및 이메일 형식 체크

--- api/models/user.js
+++ api/models/user.js
@@ -5,8 +5,13 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: {type: String},
-    email: {type: String, require: true},
-    password: {type: String, require: true}
+    email: {
+        type: String, 
+        required: true,
+        unique: true,
+        match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    },
+    password: {type: String, required: true}
 });
 
 
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+// 암호화 하기 위한 라이브러리
 const bcrypt = require('bcryptjs');
 
 const UserModel = require('../models/user');
@@ -28,37 +29,52 @@ router.get('/all', (req, res) => {
 // 유저 등록 url : localhost:3000/user/register
 router.post('/register', (req, res) => {
     
-    // password 값 첫번 째 암호화 할 값, 두번 째 자리수, 3번 째 에러가 있거나 hash가 있거나.
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).json({
+    UserModel
+        .find({email: req.body.email})
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    msg: "please check your email"
+                });
+            } else {
+                // password 값 첫번 째 암호화 할 값, 두번 째 자리수, 3번 째 에러가 있거나 hash가 있거나.
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new UserModel({
+                            _id: new mongoose.Types.ObjectId(),
+                            name: req.body.name,
+                            email: req.body.email,
+                            password: hash
+                        });
+
+                        user
+                            .save()
+                            .then(result => {
+                                res.status(200).json({
+                                    msg: "success user Created",
+                                    userInfo: result
+                                });
+                            })
+                            // 도메인 체크
+                            .catch(err => {
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
                 error: err
             });
-        } else {
-            const user = new UserModel({
-                _id: new mongoose.Types.ObjectId(),
-                name: req.body.name,
-                email: req.body.email,
-                password: hash
-            });
-
-            user
-                .save()
-                .then(result => {
-                    res.status(200).json({
-                        msg: "success user Created",
-                        userInfo: result
-                    });
-                })
-                // 도메인 체크
-                .catch(err => {
-                    res.status(500).json({
-                        error: err
-                    });
-                });
-        }
-    })
-
+        });
 });
 
 
